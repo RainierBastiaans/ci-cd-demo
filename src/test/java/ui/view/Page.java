ce40f04ac840e7eb5cd867b9ca81767363f43e93
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2022-12-19 20:31:52 +0100

    Add GitHub Actions

--- src/test/java/ui/view/Page.java
+++ src/test/java/ui/view/Page.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public abstract class Page {
 
     WebDriver driver;
-    String path = Config.BASE_URL;
+    String path = "http://localhost:8080/ci_cd_demo_war_exploded/";
 
     public Page (WebDriver driver) {
         this.driver = driver;
--- src/test/java/ui/view/WikipediaSimpleTest.java
+++ src/test/java/ui/view/WikipediaSimpleTest.java
@@ -3,31 +3,46 @@ package ui.view;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WikipediaSimpleTest {
 
-	@Test
-	public void test_Wikipedia_with_KHL_results () {
-		WebDriver driver = DriverHelper.getDriver();
-        driver.get("http://en.wikipedia.org/wiki/Main_Page");
-        
-        WebElement searchField = driver.findElement(By.id("searchInput"));
-        searchField.clear();
-        searchField.sendKeys("KHL");
-        
-        WebElement goButton = driver.findElement(By.id("searchButton"));
-        goButton.click();
+    private WebDriver driver;
 
-        String h2Text = driver.findElement(By.id("firstHeading")).getText();
-        assertEquals("Kontinental Hockey League",h2Text);
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://nl.wikipedia.org/wiki/Hoofdpagina");
+    }
 
-        String captionText = driver.findElement(By.cssSelector("caption")).getText();
-        assertEquals("Kontinental Hockey League", captionText);
-        
+
+    @After
+    public void clean(){
         driver.quit();
-	}
+    }
+
+    @Test
+    public void browserVindtWikipedia() {
+        assertEquals("Wikipedia, de vrije encyclopedie", driver.getTitle());
+    }
+
+    @Test
+    public void wikipediaVindtSelenium() {
+        WebElement field = driver.findElement(By.id("searchInput"));
+        field.clear();
+        field.sendKeys("selenium");
+        WebElement link = driver.findElement(By.id("searchButton"));
+        link.click();
+
+        assertEquals("Selenium - Wikipedia", driver.getTitle());
+
+        assertEquals("Selenium", driver.findElement(By.tagName("h1")).getText());
+    }
 
 }
