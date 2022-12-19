b7aba81b67b96bfa65bec3964b26cb4762f7d447
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2022-12-20 00:19:59 +0100

    selenium change

--- src/test/java/ui/view/DriverHelper.java
+++ src/test/java/ui/view/DriverHelper.java
@@ -3,14 +3,22 @@ package ui.view;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverHelper {
     private static WebDriver driver;
 
     public static WebDriver getDriver(){
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         return driver;
     }
