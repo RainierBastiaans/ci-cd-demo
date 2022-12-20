6895f098139454e29c80881fb8fee318a39c207c
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2022-12-20 02:04:11 +0100

    selenium change

--- src/test/java/ui/view/DriverHelper.java
+++ src/test/java/ui/view/DriverHelper.java
@@ -3,7 +3,6 @@ package ui.view;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -13,16 +12,8 @@ public class DriverHelper {
 
     public static WebDriver getDriver(){
         WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        WebDriver driver = new ChromeDriver(options);
+        ChromeDriver driver;
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         new WebDriverWait(driver, Duration.ofSeconds(60));
         return driver;
