2065887987389e93b9a65367979062be33c4de9f
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2022-12-20 01:18:52 +0100

    selenium change

--- src/test/java/ui/view/DriverHelper.java
+++ src/test/java/ui/view/DriverHelper.java
@@ -4,22 +4,27 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class DriverHelper {
     private static WebDriver driver;
 
     public static WebDriver getDriver(){
         WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver_win32(1)\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--headless");
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("start-maximized"); // open Browser in maximized mode
         options.addArguments("disable-infobars"); // disabling infobars
         options.addArguments("--disable-extensions"); // disabling extensions
         options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--no-sandbox"); // Bypass OS security model
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        new WebDriverWait(driver, Duration.ofSeconds(60));
         return driver;
     }
 }
