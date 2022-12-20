7c1d4eef7d1868452671336ca590e582c4d0b604
Rainier Bastiaans <rainier.bastiaans@student.ucll.be>
2022-12-20 03:14:13 +0100

    selenium change

--- src/test/java/ui/view/DriverHelper.java
+++ src/test/java/ui/view/DriverHelper.java
@@ -3,6 +3,7 @@ package ui.view;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,8 +13,11 @@ public class DriverHelper {
 
     public static WebDriver getDriver(){
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver;
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         new WebDriverWait(driver, Duration.ofSeconds(60));
         return driver;
